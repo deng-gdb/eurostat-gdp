eba7e4439ad29f908861741b17d277b03c5235dd
gdbsign@gmail.com <gdbsign@gmail.com>
2023-09-06 14:54:25 +0000

    Updated casted_to_numeric_nama-10r-2gdp in order to proper process empty fields.

--- dbt/models/intermediate/casted_to_numeric_nama-10r-2gdp.sql
+++ dbt/models/intermediate/casted_to_numeric_nama-10r-2gdp.sql
@@ -3,5 +3,5 @@
 select unit,
        geo,
        year,
-       cast(REGEXP_REPLACE(value, r"[^0-9.]", NULL) as numeric)  as `value`
+       cast(trim(REGEXP_REPLACE(value, r"[^0-9.]", "")) as numeric)  as `value`
   from {{ ref('unpivoted_nama-10r-2gdp') }}
