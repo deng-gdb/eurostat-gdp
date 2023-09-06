2234b76f7aca109fa66f5ce483b67ab0e1158c1b
gdbsign@gmail.com <gdbsign@gmail.com>
2023-09-06 13:24:33 +0000

    Updated facts. Added joins with dimensions and added surrogate keys in the facts.

--- dbt/models/core/dim_fact.sql
+++ dbt/models/core/dim_fact.sql
@@ -1,8 +1,58 @@
 {{ config(materialized='table', schema='core') }}
 
-select unit,
-       geo,
-       year,
-       value
-  from {{ ref('casted_to_numeric_nama-10r-2gdp') }}
- 
+
+with
+
+regions as (
+
+    select
+        region_id,
+        notation
+
+    from {{ ref('dim_regions') }}
+
+),
+
+units as (
+
+    select
+        unit_id,
+        id
+
+    from {{ ref('dim_units') }}
+
+),
+
+years as (
+
+    select
+        year_id,
+        year
+
+    from {{ ref('dim_years') }}
+
+),
+
+facts as (
+
+    select 
+        unit,
+        geo,
+        year,
+        value
+    from {{ ref('casted_to_numeric_nama-10r-2gdp') }}
+
+)
+
+select    
+    units.unit_id,
+    regions.region_id,
+    years.year_id,
+    facts.value  
+  from facts
+      inner join years
+         on facts.year = years.year
+      inner join units 
+         on units.id = facts.unit
+      inner join regions  
+         on regions.notation = facts.geo
