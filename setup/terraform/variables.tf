8fdcea03f476571e50bd2683013f90b46c068f8b
deng-gdb <143504295+deng-gdb@users.noreply.github.com>
2023-09-26 12:21:22 +0300

    Update variables.tf

--- setup/terraform/variables.tf
+++ setup/terraform/variables.tf
@@ -1,1 +1,9 @@
---
+variable "project" {
+  description = "Your GCP Project ID"
+}
+
+variable "region" {
+  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+  default = "europe-west6"
+  type = string
+}
