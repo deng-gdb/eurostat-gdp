8aff7a63d7e67d766e2cffb1710db7f9694f231b
deng-gdb <143504295+deng-gdb@users.noreply.github.com>
2023-09-27 10:19:30 +0300

    Update main.tf

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "google" {
-  project = var.project
+  project = var.GCP_PROJECT_ID
   region = var.region
   // credentials = file(var.credentials)  # Use this if you do not want to set env-var GOOGLE_APPLICATION_CREDENTIALS
 }
