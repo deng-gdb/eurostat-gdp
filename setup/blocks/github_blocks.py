a07a8d94c29a7c847d8348da200c900bed6532ae
deng-gdb <143504295+deng-gdb@users.noreply.github.com>
2023-10-06 23:03:20 +0300

    Update github_blocks.py

--- setup/blocks/github_blocks.py
+++ setup/blocks/github_blocks.py
@@ -1,10 +1,9 @@
 from prefect.filesystems import GitHub
-import block_vars
+import setup
 
 # alternative to creating GitHub block in the UI
-
 gh_block = GitHub(
-    name="eurostat-gdp-github", repository=block_vars.github_repository_url
+    name="eurostat-gdp-github", repository=setup.github_repository_url
 )
 
 gh_block.save("eurostat-gdp-github", overwrite=True)
