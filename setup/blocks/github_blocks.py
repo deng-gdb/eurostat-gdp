5c6d705141b6b7d474066966dd2d320a67facfed
deng-gdb <dmitri@DESKTOP-TQR1PLJ.localdomain>
2023-10-14 13:02:51 +0300

    Adjusted block creation

--- setup/blocks/github_blocks.py
+++ setup/blocks/github_blocks.py
@@ -6,4 +6,6 @@ gh_block = GitHub(
     name="eurostat-gdp-github", repository=setup.github_repository_url
 )
 
+gh_block.get_directory("flows") # specify a subfolder of repo where your flows code is located
+
 gh_block.save("eurostat-gdp-github", overwrite=True)
