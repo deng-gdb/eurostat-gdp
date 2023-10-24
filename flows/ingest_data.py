47adc355b394dc0758593ccdcad82ffd8bb74ae0
deng-gdb <dmitri@DESKTOP-TQR1PLJ.localdomain>
2023-10-24 15:37:15 +0300

    added dbt job run

--- flows/ingest_data.py
+++ flows/ingest_data.py
@@ -1,5 +1,6 @@
 from from_web_to_gcs import from_web_to_gcs
 from from_gcs_to_bq import from_gcs_to_bq
+from run_dbt_job import dbt_job_flow 
 from prefect import flow, task
 
 
@@ -7,8 +8,15 @@ from prefect import flow, task
 def ingest_data():
     """Main flow to ingest data from web into Big Query"""
 
+    # download dataset from the Eurostat and store in in the GCS bucket
     from_web_to_gcs()
+
+    # Upload this dataset in the BigQuery
     from_gcs_to_bq()
+    
+    #You can't use the dbt Cloud API in the dbt Cloud Free pricing plan.
+    # So, the corresponding dbt job should be run in dbt Cloud UI mannually
+    # run_dbt_job()
 
 
 
